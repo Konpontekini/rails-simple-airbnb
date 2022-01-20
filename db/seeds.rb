4da79c94dc577e4e291ec3fd41d29756d4943229
thomas cuvelier <thomas_cuvelier@hotmail.com>
2022-01-20 22:28:00 +0000

    completed but ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Tiny flat in London',
+  address: '50 Radcliffe Place  London E9 5EF',
+  description: 'Very tiny flat in the middle of nowhere',
+  price_per_night: 25,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'A warehouse with no bed',
+  address: '8 Industrial Parl London NW1 7BE',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 85,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Flat for Royalty in London',
+  address: 'Buckingham Palace, London W1W 7BB',
+  description: 'Live amongst Royalty at very expensive price',
+  price_per_night: 375,
+  number_of_guests: 2
+)
